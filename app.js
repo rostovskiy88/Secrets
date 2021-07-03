c31475448b218340b46f4c0f83fa365e2d07e7c3
unknown <rostovskiyoleg88@gmail.com>
2021-07-03 13:16:03 +0300

    Add Enviroment vars

---
+++ app.js
@@ -0,0 +1,93 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+console.log(process.env.API_KEY);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt, {
+    secret: process.env.SECRET,
+    encryptedFields: ['password']
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({
+        email: username
+    }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets")
+                } else {
+                    res.send("Wrong username or password!")
+                }
+            };
+        }
+
+    });
+});
+
+
+
+
+app.listen(port, () => console.log("Server started on port 3000"));
