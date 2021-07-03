281d2f59b5766d724a10ed8152452a102d8df83a
unknown <rostovskiyoleg88@gmail.com>
2021-07-03 21:31:52 +0300

    added bcrypt

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 const port = process.env.PORT || 3000;
 
@@ -29,13 +28,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-userSchema.plugin(encrypt, {
-    secret: process.env.SECRET,
-    encryptedFields: ['password']
-});
-
-
 const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -51,18 +43,22 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
     });
 
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
-    });
 });
 
 app.post("/login", (req, res) => {
@@ -76,14 +72,15 @@ app.post("/login", (req, res) => {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets")
-                } else {
-                    res.send("Wrong username or password!")
-                }
-            };
-        }
-
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render("secrets")
+                    } else {
+                        res.send("Wrong username or password!")
+                    }
+                });
+            }
+        };
     });
 });
 
